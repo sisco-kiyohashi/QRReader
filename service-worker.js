35c0151a10e01a0231912387294c124b0501388d
sisco-kiyohashi <m-kiyohashi@sisco.co.jp>
2023-07-26 13:05:40 +0900

    初期登録

---
+++ service-worker.js
@@ -0,0 +1,33 @@
+// Service Workerの登録
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
+
+// キャッシュの追加
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('cache-name').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/styles.css',
+        '/script.js'
+      ]);
+    })
+  );
+});
+
+// オフライン時の動作
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
